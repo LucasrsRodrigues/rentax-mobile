f8808508aff2131cf6097ab4c16bc59d1aeeb538
Lucas Ridrigues Santos <lucasrs.rodrigues3@outlook.com>
2021-07-08 08:42:03 -0300

    add

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import * as S from './styles';
 
-export function Home(){
-  return(
-   <S.Container>
+export function Home() {
 
-   </S.Container>
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 240,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png' 
+  };
+
+  const carDataTwo = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 240,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png' 
+  };
+
+  return (
+    <S.Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <S.Header>
+        <S.HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <S.TotalCars>
+            Total de 12 carros
+          </S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
+    </S.Container>
   );
 }
